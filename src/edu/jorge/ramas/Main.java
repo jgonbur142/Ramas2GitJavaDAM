7111022bf352ac9848caf57ae256d195ba9f8bd1
jgonbur142 <jgonbur142@g.educaand.es>
2025-11-14 14:09:55 +0100

    Error durante la practica

--- src/edu/jorge/ramas/Main.java
+++ src/edu/jorge/ramas/Main.java
@@ -15,7 +15,5 @@ public class Main {
         int complementario = r.nextInt(49) + 1;
         System.out.println("Complementario: " + complementario);
         
-        int suma = bolas.stream().mapToInt(Integer::intValue).sum();
-        System.out.println("Suma de principales: " + suma);
     }
 }
